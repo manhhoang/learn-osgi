ed2b0a3abfe1d0dd0409648f76b5a5eb22b5a7a1
Manh Ha <manhh@OH-manhh-MBP.orion.internal>
2017-12-11 12:12:26 +1300

    init project

---
+++ src/com/mh/learn_osgi/Activator.java
@@ -0,0 +1,13 @@
+package com.mh.learn_osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+    public void start(BundleContext context) throws Exception {
+        System.out.println("Hello world");
+    }
+    public void stop(BundleContext context) throws Exception {
+        System.out.println("Goodbye World");
+    }
+}
